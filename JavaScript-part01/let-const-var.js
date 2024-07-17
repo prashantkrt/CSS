aa38fa5b04834ac619ae7e0e8d3dd849a216d400
prashanttiwary10@gmail.com <prashanttiwary10@gmail.com>
2024-07-17 21:38:16 +0530

    js part 01

---
+++ JavaScript-part01/let-const-var.js
@@ -0,0 +1,25 @@
+let a = 10;
+console.log(a);
+// Not allowed let a =20;
+
+const c =10;
+// Not allowed c = c+10 cannot update it
+console.log(c);
+
+
+var b = 20;
+// you can re declare but it's a bad practice old approachs use let 
+var b = 30;
+console.log(b)
+
+
+
+let val = 20;
+console.log(val);
+
+{
+    //local
+    let val = 30;
+    console.log(val);
+}
+
