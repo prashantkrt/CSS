f16a79f36d66618bae8de94e740169295e4e9314
prashanttiwary10@gmail.com <prashanttiwary10@gmail.com>
2024-07-17 18:26:31 +0530

    js part 01

---
+++ JavaScript-part01/NaN.js
@@ -0,0 +1,18 @@
+// Not-a-Number
+
+// 0 / 0 
+console.log(0/0);
+
+// NaN - 1 
+console.log(NaN-1);
+
+// NaN * 2 
+console.log(NaN-1);
+
+// NaN + NaN
+console.log(NaN + NaN);
+
+let n = NaN
+// typeof number 
+// but no a valid number 
+console.log(typeof n);
---
+++ JavaScript-part01/operations.js
@@ -0,0 +1,17 @@
+let a=20;
+let b=30;
+
+console.log(a+b);
+
+console.log(a-b);
+
+console.log(a*b);
+
+console.log(a/b);
+
+a=2
+b=3
+//power operator 
+console.log(a**b);
+
+console.log(a%b);
