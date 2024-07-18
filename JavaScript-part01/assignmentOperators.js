eb99396b3a2a20406ee5df770bcb4904e11aa2cf
prashanttiwary10@gmail.com <prashanttiwary10@gmail.com>
2024-07-18 10:43:38 +0530

    js part 01

---
+++ JavaScript-part01/assignmentOperators.js
@@ -0,0 +1,20 @@
+let age = 9;
+
+age = age + 1;
+age += 1;
+
+age = age * 2;
+age *= 2;
+
+age = age - 1;
+age -= 1;
+
+age = aga / 2;
+age /= 2;
+
+
+//Uninary Operatos
+age++;  //post increment 
+age--;  // post decrement
+++age;  // pre increment
+--age;  // pre decrement
