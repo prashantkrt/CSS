b1e04963e1c270ca5a06f08f51cf40542eceee68
prashanttiwary10@gmail.com <prashanttiwary10@gmail.com>
2024-07-18 15:43:21 +0530

    js part01

---
+++ JavaScript-part01/Identifiers-rules.js
@@ -0,0 +1,5 @@
+// Name can contains letters , digits ,underscores and dollar signs. (No Space) 
+// Name can begin with letters , $ and _
+// Names are case sensitive
+// Reserved cannot be used as names 
+// camelCase 
