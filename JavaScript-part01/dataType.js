57fb3f0c417f139e88f7485b62b8afc520cba221
prashanttiwary10@gmail.com <prashanttiwary10@gmail.com>
2024-07-18 18:48:10 +0530

    js part 01

--- JavaScript-part01/dataType.js
+++ JavaScript-part01/dataType.js
@@ -3,24 +3,37 @@ a = 20;
 console.log(typeof a);
 
 //Number
-let b= 2.8
+let b = 2.8;
 console.log(typeof b);
 
-//boolean 
+//boolean
 flag = true;
-console.log(typeof flag)
+console.log(typeof flag);
+// flag= True // True is not defined error
+// console.log(flag);
 
-//String
-coderName = "Prashant"
-console.log(typeof coderName)
+//string
+coderName = "Prashant";
+//string indices
+console.log(coderName[0]);
+//string length property
+console.log(coderName.length)
+//gives new  string
+let coder = coderName + " Fullstack Developer";
+console.log(coderName);
+console.log(coder);
+console.log(typeof coderName);
 
-//string 
-let data ='a';
+//string
+let data = "a";
+data = "23";
+console.log(data[0]);
+data = "";
 console.log(typeof data);
 
 //undefined
 let x;
-let userName = undefined
+let userName = undefined;
 console.log(typeof x);
 console.log(typeof userName);
 
@@ -28,20 +41,20 @@ console.log(typeof userName);
 let number = null;
 console.log(typeof number);
 
-//not much used :
+//not much used : BigInt and Symbol()
 
 //Bigint
 let bb = BigInt("1233343");
 console.log(typeof bb);
 
 // BigInt value
-// A BigInt number is created by appending n to the end of an integer. 
+// A BigInt number is created by appending n to the end of an integer.
 let value1 = 900719925124740998n;
-console.log(typeof value1)
+console.log(typeof value1);
 
 // add two big integers
 let result1 = value1 + 1n;
-console.log(result1);  // "900719925124740999n"
+console.log(result1); // "900719925124740999n"
 
 let value2 = 900719925124740998n;
 console.log(typeof value2);
@@ -54,25 +67,22 @@ console.log(typeof s);
 let val1 = Symbol("programiz");
 let val2 = Symbol("programiz");
 
-//compare value and type 
-console.log(val1 === val2);  // false
+//compare value and type
+console.log(val1 === val2); // false
 
-//object 
+//object
 let Student = {
-    firstName: "Rohit",
-    lastName: "Kumar",
-    class: 10
+  firstName: "Rohit",
+  lastName: "Kumar",
+  class: 10,
 };
 
 console.log(Student);
 //object
 console.log(typeof Student);
-console.log(Student.firstName)
+console.log(Student.firstName);
 console.log(Student["firstName"]);
 
-Student.firstName="Amar";
+Student.firstName = "Amar";
 Student.lastName = "Khanna";
 console.log(Student);
-
-
-
