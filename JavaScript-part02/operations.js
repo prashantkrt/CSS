df4bf76b992bee2ac7d1698ba1ea4c3ab5f1810e
prashanttiwary10@gmail.com <prashanttiwary10@gmail.com>
2024-07-18 21:27:12 +0530

    js part 02

---
+++ JavaScript-part02/operations.js
@@ -0,0 +1,62 @@
+//Arithmetic 
+
+let a = 30;
+let b = 20; 
+
+console.log(a+b);
+console.log(a-b);
+console.log(a*b);
+console.log(a/b);
+console.log(a%b);
+a=2;
+b=3;
+console.log(a**b);
+
+//Unary Operator
+
+let val = 20;
+
+console.log(val++);
+console.log(++val);
+console.log(val--);
+console.log(--val);
+
+
+//Assignments
+
+a=20;
+b=10;
+
+a+=20;
+b-=2;
+a/=2;
+a%=2;
+
+//comparison 
+let age = 19;
+console.log(age>18);//true
+console.log(age>=18);//true
+console.log(age<18);//false
+console.log(age<=18);//false
+console.log(age==18);//false
+console.log(age!=18);//true
+
+
+let value1 = 5
+console.log(typeof value1); // number
+let value2 ='5'
+console.log(typeof value2);  // string
+
+console.log(value1==value2)// true  , since it's only check the values not the type
+console.log(value1===value2) // false , here it's check type as well as the value 
+
+
+console.log(0==' ') // true
+console.log(0===' ') // false
+console.log(0==false) //true
+console.log(1==true) //true
+console.log(1===true) //false 
+console.log(0===false) //false
+console.log(null==undefined) // true
+console.log(null===undefined)// false
+
---
+++ JavaScript-part02/template-literals.js
@@ -0,0 +1,11 @@
+let a = 5;
+let b = 10;
+console.log(`your pay ${a + b} rupees`);
+
+let pensilPrice = 20;
+let eraserPrice = 30;
+
+console.log("Total is is ", pensilPrice + eraserPrice ," rupees");
+
+//template-literals
+console.log(`Total prices ${pensilPrice + eraserPrice} rupees !!!`);
