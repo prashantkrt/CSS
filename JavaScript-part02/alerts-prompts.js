f6e5243d03f9d92e308d4c0a92d226c9939799e7
prashanttiwary10@gmail.com <prashanttiwary10@gmail.com>
2024-07-19 11:44:40 +0530

    Javascript part02

--- JavaScript-part02/alerts-prompts.js
+++ JavaScript-part02/alerts-prompts.js
@@ -6,3 +6,9 @@ alert("Someting is wrong!!!");
 console.log("Logging")
 console.error("Error");
 console.warn("Warning")
+
+
+let name=prompt("please enter your name");
+let age = prompt("Please enter your age");
+
+alert(`${name} is ${age} years old`);
