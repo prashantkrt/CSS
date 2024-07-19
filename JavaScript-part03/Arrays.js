fd4ee6f935d213971bfe43fce60c1803493be4cf
prashanttiwary10@gmail.com <prashanttiwary10@gmail.com>
2024-07-19 21:07:18 +0530

    Javascript part03

--- JavaScript-part03/Arrays.js
+++ JavaScript-part03/Arrays.js
@@ -15,9 +15,45 @@ let names = ["Boby","Rony","John"];
 
 let info =["Aman", 23, 45, "Roshan", 4.5, 9n]; // mixed array
 console.log(info);
-console.log(info[0][0]) //A
+console.log(info[0][0]) // A
 console.log(info[0].length);
 
 // Arrays are mutable
+let fruits = ["apple","orange","banana","pineapple"];
+fruits[0]="Guava";
+console.log(fruits);
+
+fruits[10]="Grapes"; // increases the length to length 11
+console.log(fruits.length);
+console.log(fruits);
+
+
+// Arrays Methods 
+
+// Push : add to end 
+// Pop : delete from the end
+// Unshift : add to start
+// Shift : delete from start
+
+let cars =["Audi","BMW","Hyundau","Tata","Suzuki"];
+
+console.log(cars.push("Mahindra")); // 6
+cars.push("Citrozen"); // 7
+console.log(cars);
+
+cars.pop(); //returns the last deleted element
+console.log(cars.pop());
+console.log(cars);
+
+console.log(cars.unshift("Ford")); // 6
+console.log(cars);
+
+cars.shift(); // returns the first deleted element
+console.log(cars.shift());
+console.log(cars);
+
+
+
+
 
 
