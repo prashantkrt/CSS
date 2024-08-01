829f405f490e92efeed6e12efc8e7bb34d287552
prashanttiwary10@gmail.com <prashanttiwary10@gmail.com>
2024-08-01 22:16:39 +0530

    JavaScript-Part08

--- JavaScript-part08/Maps-Filter.js
+++ JavaScript-part08/Maps-Filter.js
@@ -1,5 +1,10 @@
 let num =[1,2,3,4];
 
-let double = num.map(function(element){
+// gives new array not make changes on the original array
+let numDouble = num.map(function(element){
     return element*2;
+})
+
+numDouble.forEach((element)=>{
+ console.log(element);
 })
