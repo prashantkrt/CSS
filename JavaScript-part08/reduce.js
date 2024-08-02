92e988cddceac0b8a5f3ff986258382460e62835
prashanttiwary10@gmail.com <prashanttiwary10@gmail.com>
2024-08-02 22:13:14 +0530

    JavaScript-Part08

--- JavaScript-part08/reduce.js
+++ JavaScript-part08/reduce.js
@@ -1,4 +1,16 @@
 // Reduces array to the single value
-
-let sum = [1,2,3,4].reduce((result,element)=>(result+element));
+let sum = [1, 2, 3, 4].reduce((result, element) => result + element);
 console.log(sum);
+
+// Finding the maximum element in the array
+let nums = [2, 3, 4, 1, 7, 8, 11, 19, 30, 32, 56, 0, 12];
+
+let maxElement = nums.reduce((max, element) => {
+  if (element > max) {
+    return element;
+  } else {
+    return max;
+  }
+});
+
+console.log(maxElement);
